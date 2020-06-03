b808ab685459f97b9c8a0b3068a09fd2a7a9f839
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-03 16:11:37 -0400

    deploy

---
+++ precache-manifest.8fd32abfc1f683ebb5ab93871310eb17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec54b2e95dcf69cabf4cf7a0b5db114",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241122608e319593e210",
+    "url": "/static/css/main.68e5d947.chunk.css"
+  },
+  {
+    "revision": "ad2fb3ef9cf2992aa166",
+    "url": "/static/js/2.f3095566.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.f3095566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241122608e319593e210",
+    "url": "/static/js/main.7bc85294.chunk.js"
+  },
+  {
+    "revision": "fccfad057589fb67256e",
+    "url": "/static/js/runtime-main.f498fd1a.js"
+  }
+]);
